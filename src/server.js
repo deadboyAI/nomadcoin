7514db554c1ff0d59b59a51cd68a271ce3f5a7da
deadboyai <deadboysai@gmail.com>
2018-04-15 19:09:53 +0900

    API to see unconfirmed Transactions

--- src/server.js
+++ src/server.js
@@ -3,11 +3,14 @@ const express = require("express"),
   morgan = require("morgan"),
   Blockchain = require("./blockchain"),
   P2P = require("./p2p"),
+  Mempool = require("./mempool"),
   Wallet = require("./wallet");
 
 const { getBlockchain, createNewBlock, getAccountBalance, sendTx } = Blockchain;
 const { startP2PServer, connectToPeers } = P2P;
 const { initWallet } = Wallet;
+const { getMempool } = Mempool;
+
 const PORT = process.env.HTTP_PORT || 3000;
 
 const app = express();
@@ -38,7 +41,9 @@ app.get("/me/balance", (req, res) => {
 
 app
   .route("/transactions")
-  .get((req, res) => {})
+  .get((req, res) => {
+    res.send(getMempool());
+  })
   .post((req, res) => {
     try {
       const { body: { address, amount } } = req;
